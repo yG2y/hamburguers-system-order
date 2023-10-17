dff5138665d0ce7c643760d5f42797feb5459370
G2 <g2002souzajardim@gmail.com>
2023-10-17 17:17:40 -0300

    criação de formulário e estilização

---
+++ src/components/BurguerForm.vue
@@ -0,0 +1,117 @@
+<template >
+    <div>
+        <p>Componente de Mensagem</p>
+        <div>
+            <form id="burguer-form">
+                <div class="input-container">
+                    <label for="nome">Nome do cliente:</label>
+                    <input type="text" name="nome" id="nome" v-model="nome" placeholder="Digite o seu nome">
+                </div>
+                <div class="input-container">
+                    <label for="pao">Escolhao o pão:</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione o seu pão</option>
+                        <option value="integral">integral</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="carne">Escolhao a carne do seu burguer:</label>
+                    <select name="carne" id="carne" v-model="carne">
+                        <option value="">Selecione o tipo de carne</option>
+                        <option value="maminha">Maminha</option>
+                    </select>
+                </div>
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>Salame</span>
+                    </div>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>Salame</span>
+                    </div>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>Salame</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar meu Burguer!">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BurguerForm',
+}
+</script>
+<style scoped>
+#burguer-form {
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+.input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+
+label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #FCBA03;
+}
+
+input, select {
+    padding: 5px 10px;
+    width: 300px;
+}
+
+#opcionais-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+#opcionais-title {
+    width: 100%;
+}
+
+.checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+}
+
+.checkbox-container span, .checkbox-container input{
+    width: auto;
+}
+
+.checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+}
+
+.submit-btn {
+    background-color: #222;
+    color: #FCBA03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+}
+
+.submit-btn:hover {
+    background-color: #FCBA03;
+    color: #222;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu burguer:</h1>
-    <p>formulário</p>
+    <BurguerForm />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue';
+import BurguerForm from '@/components/BurguerForm.vue';
 
 export default {
   name: 'Home',
   components: {
     Banner,
-  },
+    BurguerForm
+},
 };
 
 </script>
